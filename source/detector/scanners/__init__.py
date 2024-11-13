7fdc61fde85c2ef63c8d33865c13fe3d5ef4abaa
Mati <ratajczak.mat@gmail.com>
2024-11-13 22:38:57 +0100

    Initial commit with basic project structure

---
+++ source/detector/scanners/__init__.py
@@ -0,0 +1,11 @@
+#  ---------------------------------------------------------------------------------------------------------------------
+# Name:             detector.scanners.__init__
+# Created By :      marataj
+# Created Date:     2024-11-13
+#  ---------------------------------------------------------------------------------------------------------------------
+
+"""
+Package including modules responsible for scanning the URLs using different detection engines.
+
+"""
+__all__ = []
